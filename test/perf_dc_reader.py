7fdec8d3f7022d81e40a8661249937a82e124a25
Joseph vantassel <jvantassel@utexas.edu>
2020-01-22 09:06:26 -0600

    :racehorse: Add performance tests for readers

---
+++ test/perf_dc_reader.py
@@ -0,0 +1,22 @@
+"""This file contains a performance test for reading a file containing
+a dispersion curve."""
+
+import swipp
+from testtools import get_full_path
+import cProfile
+import pstats
+
+full_path = get_full_path(__file__)
+
+def main():
+    suite = swipp.DispersionSuite.from_geopsy(full_path+"data/test_dc_mod100_ray2_lov2_full.txt")
+
+fname = full_path+"data/.tmp_profiler_run"
+data = cProfile.run('main()', filename=fname)
+stat = pstats.Stats(fname)
+stat.sort_stats('tottime')
+stat.print_stats(0.1)
+
+# YEAR - MO - DY : TIME UNIT
+# -------------------------
+# 2020 - 01 - 22 :  0.141s
---
+++ test/perf_gm_reader.py
@@ -0,0 +1,22 @@
+"""This file contains a performance test for reading a file containing
+a dispersion curve."""
+
+import swipp
+from testtools import get_full_path
+import cProfile
+import pstats
+
+full_path = get_full_path(__file__)
+
+def main():
+    suite = swipp.GroundModelSuite.from_geopsy(full_path+"data/test_gm_mod100.txt")
+
+fname = full_path+"data/.tmp_profiler_run"
+data = cProfile.run('main()', filename=fname)
+stat = pstats.Stats(fname)
+stat.sort_stats('tottime')
+stat.print_stats(0.1)
+
+# YEAR - MO - DY : TIME UNIT
+# -------------------------
+# 2020 - 01 - 22 :  0.019s
