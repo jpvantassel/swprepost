5d1b0c5da9b7b82bbe385e97502ab960d8f8f751
Joseph vantassel <jvantassel@utexas.edu>
2020-03-19 16:47:15 -0500

    :bug: Fix, remove assert from testtools

--- test/testtools.py
+++ test/testtools.py
@@ -1,3 +1,22 @@
+# This file is part of SigProPy, a Python package for digital signal
+# processing.
+# Copyright (C) 2019-2020 Joseph P. Vantassel (jvantassel@utexas.edu)
+#
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https: //www.gnu.org/licenses/>.
+
+"""Testing tools."""
+
 import unittest
 
 def get_full_path(path):
@@ -19,7 +38,8 @@ class TestCase(unittest.TestCase):
         self.assertListEqual(array1.tolist(), array2.tolist())
 
     def assertArrayAlmostEqual(self, array1, array2, **kwargs):
-        assert(array1.size == array2.size)
+        if array1.size != array2.size:
+            self.assertEqual(array1.size, array2.size)
         array1 = array1.flatten()
         array2 = array2.flatten()
         for v1, v2 in zip(array1, array2):
